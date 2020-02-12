7ee129010c7d2444d7f855615468429d1db5a9ca
Mandy Meindersma <meinders@ualberta.ca>
2020-02-12 07:13:22 -0700

    I can read the numbers from the rotary phone properly now

--- rotary-phone.py
+++ rotary-phone.py
@@ -1,6 +1,6 @@
 # must run as sudo because pi doesn't actually have access to the pins, only sudo does
 
-from time import sleep
+import time
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD) # uses the physical pin numbering system
@@ -9,11 +9,25 @@ button = 8
 #third param is an internal  resistor
 GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+CURRENT = 1
+GROUND = 0
+previous_state = CURRENT
+count = 0
+time_at_switch = time.time()
+
 while (True):
+    
     # if the button is pressed
     if GPIO.input(button) == 0:
-        print("pressed")
-        sleep(0.1)
+        state = CURRENT
     else:
-        print("hit number")
-        sleep(0.1)
+        state = GROUND
+    time.sleep(0.02)
+    if previous_state != state:
+        time_at_switch = time.time()
+        count+=1
+        print("switch" + str(count/2%10))
+    if time.time() - time_at_switch > 1 and count != 0:
+        print("we have mmoved on to new number")
+        count = 0
+    previous_state = state
